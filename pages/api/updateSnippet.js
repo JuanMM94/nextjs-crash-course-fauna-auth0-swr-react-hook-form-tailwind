ecbe05daa75c5180fcb315f801e62ff345088c5f
Juan Martin <juanmartinmonasterio@gmail.com>
2022-11-30 12:42:33 -0300

    1

--- pages/api/updateSnippet.js
+++ pages/api/updateSnippet.js
@@ -1,14 +1,22 @@
-import { updateSnippet } from '../../utils/Fauna';
+import { updateSnippet } from "../../utils/Fauna";
 export default async function handler(req, res) {
-    if (req.method !== 'PUT') {
-        return res.status(405).json({ msg: 'Method not allowed' });
-    }
-    const { id, code, language, description, name } = req.body;
+  if (req.method !== "PUT") {
+    return res.status(405).json({ msg: "Method not allowed" });
+  }
+  const { id, code, language, description, name } = req.body;
 
-    try {
-        //TODO: update snippet
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ msg: 'Something went wrong.' });
-    }
+  try {
+    //TODO: update snippet
+    const updatedSnippet = await updateSnippet(
+      id,
+      code,
+      language,
+      description,
+      name
+    );
+    return res.status(200).json(updateSnippet);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Something went wrong." });
+  }
 }
